61db10638c0398e5e9748445f0d96f418dd37899
timur2912 <gattarov.ta@phystech.edu>
2024-07-16 12:42:30 +0300

    prepared for deploy

--- MIPTpython/runserver.sh
+++ MIPTpython/runserver.sh
@@ -1,5 +1,7 @@
-while true; do
+#!/bin/bash
+
+while [ "$input" != finish ]; do
   echo "Re-starting Django runserver"
-  python manage.py runserver
-  sleep 2
+  python3 manage.py runserver
+  sleep 0.5
 done
